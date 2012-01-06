a94bd48c03802eec8eedcb8fb28b6f950c8b3bf5
Guilherme Souza <guilhermef@corp.globo.com>
2012-01-06 17:53:54 -0200

    logging

--- rockload_client/cli.py
+++ rockload_client/cli.py
@@ -11,7 +11,7 @@ from uuid import uuid4
 import shutil
 
 import commands
-
+import logging
 # from fabric.api import local, lcd, settings
 
 DEFAULT_PORT = 3333
@@ -19,6 +19,9 @@ DEFAULT_IP = '0.0.0.0'
 
 
 def main():
+    log_path = "%s/logs/rock-cli-%s.log" % (os.environ['HOME'], os.getpid())
+    logging.basicConfig(filename=log_path,level=logging.DEBUG)
+    
     parser = optparse.OptionParser(usage="rockload-client or type rockload-client -h (--help) for help", description=__doc__, version="0.1.0")
     parser.add_option("-p", "--port", type="int", dest="port", default=DEFAULT_PORT, help = "The port where rockload-server is [default: %default]." )
     parser.add_option("-i", "--host", dest="host", default=DEFAULT_IP, help = "The ip address of rockload-server [default: %default]." )
@@ -31,12 +34,12 @@ def main():
         task_details = fetch(server_url, '/next-task')
         if 'task-details' in task_details:
             details = task_details['task-details']
-            print 'found task - %s' % details['url']
+            logging.debug('found task - %s' % details['url'])
             result = process_task(server_url, details)
             if not post_results(server_url, details, result).read() == 'OK':
-                print 'Failed sending results to server for test %s' % details['result_id']
+                logging.debug('Failed sending results to server for test %s' % details['result_id'])
             else:
-                print 'Posted! Now go take a look at your RockLoad server!'
+                logging.debug('Posted! Now go take a look at your RockLoad server!')
         time.sleep(5)
 
 def update_data(server_url, task_details, cloned, in_progress):
@@ -49,7 +52,7 @@ def update_data(server_url, task_details, cloned, in_progress):
 
 
 def post_results(server_url, task_details, result):
-    print 'Trying to post results'
+    logging.debug('Trying to post results')
     return urlopen('%s/post-results' % server_url, urlencode({
         'result_id': task_details['result_id'],
         'run_id': task_details['run_id'],
@@ -68,32 +71,34 @@ def process_task(server_url, task_details):
     update_data(server_url, task_details, True, False)
 
     while cant_start_task(server_url, task_details):
-        print "waiting for server to allow starting task..."
+        logging.debug("waiting for server to allow starting task...")
         time.sleep(1)
     
     xml_text = ''
     update_data(server_url, task_details, True, True)
     bench_path = '/tmp/rockload/%s/bench' % repo_id
     command = "cd %s; " % (bench_path) + "fl-run-bench -u %(url)s -c %(cycles)s -D %(duration)s --simple-fetch %(test_module)s %(test_class)s" % (task_details)
-    print commands.getoutput(command)
+    output = commands.getoutput(command)
+    logging.debug(output)
+    
     xml_text = open(join(bench_path, 'funkload.xml')).read()
-    print 'Cleaning Temp folder'
+    logging.debug('Cleaning Temp folder')
     shutil.rmtree('/tmp/rockload/%s' % repo_id)
     return xml_text
-
 
 def download_from_git(git_repo):
     repo_id = uuid4()
     if not exists('/tmp/rockload'):
         os.makedirs('/tmp/rockload')
-    print commands.getoutput('git clone %s /tmp/rockload/%s' % (git_repo, repo_id))
+    output = commands.getoutput('git clone %s /tmp/rockload/%s' % (git_repo, repo_id))
+    logging.debug(output)
     return repo_id
 
 
 def fetch(server_url, url):
     result = urlopen(join(server_url.rstrip('/'), url.lstrip('/')))
     if result.getcode() != 200:
-        print 'URL %s%s was not found!' % (server_url, url)
+        logging.debug('URL %s%s was not found!' % (server_url, url))
         return {}
 
     return loads(result.read())
